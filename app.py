135a10bf89abcf698e74dbf93dc0a88a51688c39
Takanori Suzuki <takanori@takanory.net>
2021-06-17 15:12:20 +0900

    add choice action

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import os
+import random
+import time
+
 from slack_bolt import App
 
 # Initializes your app with your bot token and signing secret
@@ -16,7 +19,7 @@ def message_hello(message, say):
         blocks=[
             {
                 "type": "section",
-                "text": {"type": "mrkdwn", "text": f"Hey there <@{message['user']}>!"},
+                "text": {"type": "mrkdwn", "text": f"こんにちは <@{message['user']}>!"},
                 "accessory": {
                     "type": "button",
                     "text": {"type": "plain_text", "text": "Click Me"},
@@ -24,7 +27,7 @@ def message_hello(message, say):
                 }
             }
         ],
-        text=f"Hey there <@{message['user']}>!"
+        text=f"こんにちは <@{message['user']}>!"
     )
 
 
@@ -33,6 +36,60 @@ def action_button_click(body, ack, say):
     # Acknowledge the action
     ack()
     say(f"<@{body['user']['id']}> clicked the button")
+
+
+@app.message(r"^\$choice")
+def choice(message, say):
+    """$choice が指定されるたらテキストエリアとボタンを表示する"""
+    blocks = [
+        {
+            "type": "input",
+            "element": {
+                "type": "plain_text_input",
+                "action_id": "choice_text",
+            },
+            "label": {
+                "type": "plain_text",
+                "text": "選択肢をスペース区切りで入力してね",
+                "emoji": True,
+            }
+        },
+        {
+            "type": "actions",
+            "elements": [
+                {
+                    "type": "button",
+                    "text": {
+                        "type": "plain_text",
+                        "text": "送信",
+                        "emoji": True,
+                    },
+                    "value": "send",
+                    "action_id": "choice_action"
+                }
+            ]
+        }
+    ]
+    say(blocks=blocks, text="選択肢をスペース区切りで入力してね")
+
+
+@app.action("choice_action")
+def choice_action(ack, body, respond, say):
+    """指定されたテキストから1つ単語を選択して返すアクション"""
+    ack()
+    values = body['state']['values']
+    for k, v in values.items():
+        value = v['choice_text']['value']
+        break
+    text = f"「{value}」が指定されました\n"
+    for i in range(5):  # 5秒sleepして5回送信する
+        choiced = random.choice(value.split())
+        text += f"{i+1}回目は「{choiced}」が選ばれました\n"
+        # respondを使うと元のメッセージが更新される
+        respond(text)
+        # 普通のsayを使ったメッセージ送信もできる
+        say(f"{i+1}回目は「{choiced}」が選ばれました")
+        time.sleep(5)
 
 
 @app.message("こんにちは")
